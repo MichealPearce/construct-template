f44589f81abd330b43c63c3397d66079fe582776
Micheal Pearce <michealpearce@outlook.com>
2022-12-29 13:36:44 -0500

    removed test code

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,17 +2,9 @@ import { registerAPI } from '@construct/server/api'
 import { registerClient } from '@construct/server/plugins/client'
 import { registerDatabase } from '@construct/server/plugins/database'
 import { registerMailer } from '@construct/server/plugins/mailer'
-import { TestTemplate } from '@construct/server/templates/TestTemplate'
 import fastify from 'fastify'
 
 async function start() {
-	const test = new TestTemplate()
-	console.log(
-		await test.render({
-			name: 'Micheal',
-		}),
-	)
-
 	const app = fastify({
 		ignoreTrailingSlash: true,
 		logger: true,
