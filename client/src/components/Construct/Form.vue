fe931e2526dd994a757b4c56e243793318034f58
Micheal Pearce <michealpearce@outlook.com>
2023-01-02 14:43:25 -0500

    init form component

---
+++ client/src/components/Construct/Form.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+	name: 'ConstructForm',
+})
+</script>
+
+<script setup lang="ts">
+const emit = defineEmits<{
+	(event: 'submit', data: FormData): void
+}>()
+
+const handleSubmit = (event: Event) =>
+	emit('submit', new FormData(event.target as HTMLFormElement))
+</script>
+
+<template>
+	<form
+		class="construct-form"
+		@submit.prevent="handleSubmit"
+	>
+		<slot />
+	</form>
+</template>
+
+<style lang="scss" scoped>
+.construct-form {
+}
+</style>
