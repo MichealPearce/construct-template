44d0fe138a56d617961d8ba8e5af99b23e4440ce
Micheal Pearce <michealpearce@outlook.com>
2023-01-05 15:33:36 -0500

    updates

--- client/src/pages/index.vue
+++ client/src/pages/index.vue
@@ -1,7 +1,9 @@
 <script lang="ts">
 import { authed } from '@construct/client/middleware/authed'
 import { useAPI } from '@construct/client/plugins/api'
-import { defineComponent } from 'vue'
+import { useAuth } from '@construct/client/stores/auth'
+import { provideUser, useUsers } from '@construct/client/stores/users'
+import { computed, defineComponent, onMounted } from 'vue'
 
 export default defineComponent({
 	name: 'IndexPage',
@@ -10,22 +12,63 @@ export default defineComponent({
 </script>
 
 <script setup lang="ts">
-const api = useAPI()
+const auth = useAuth()
+const users = useUsers()
 
-async function login() {
-	api.get('users').then(console.log).catch(console.error)
+const user = computed(() => auth.current)
+
+async function upload(data: FormData) {
+	if (!auth.current) return
+
+	try {
+		const res = await users.uploadAvatar(auth.current, data)
+		console.log(res)
+	} catch (error) {
+		console.error(error)
+	}
 }
+
+async function deleteAvatar() {
+	if (!auth.current) return
+
+	try {
+		const res = await users.deleteAvatar(auth.current)
+		console.log(res)
+	} catch (error) {
+		console.error(error)
+	}
+}
+
+onMounted(() => {
+	if (!auth.current) return
+
+	users.fetchAvatar(auth.current).then(console.log).catch(console.error)
+})
+
+provideUser(user)
 </script>
 
 <template>
 	<ConstructPage class="index-page">
 		<h1>Hello</h1>
 
-		<ConstructButton @click="login">Test</ConstructButton>
+		<UserAvatar />
+		<ConstructForm @submit="upload">
+			<ConstructInput
+				name="avatar"
+				type="file"
+			/>
+			<ConstructButton type="submit">Upload</ConstructButton>
+		</ConstructForm>
+
+		<ConstructButton @click="deleteAvatar">Delete</ConstructButton>
 	</ConstructPage>
 </template>
 
 <style lang="scss" scoped>
 .index-page {
+	img {
+		max-width: 100%;
+	}
 }
 </style>
