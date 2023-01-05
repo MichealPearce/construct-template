f8b4d82ded5168a819263fa204212d41bfbeaaaf
Micheal Pearce <michealpearce@outlook.com>
2023-01-05 16:25:48 -0500

    create test page

---
+++ client/src/pages/test.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+	name: 'TestPage',
+})
+</script>
+
+<script setup lang="ts">
+const opened = ref(false)
+</script>
+
+<template>
+	<ConstructPage class="test-page">
+		<ConstructMenuButton v-model="opened" />
+	</ConstructPage>
+</template>
+
+<style lang="scss" scoped>
+.test-page {
+}
+</style>
